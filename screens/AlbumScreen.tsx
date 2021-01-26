e787c9e07ad5662e81210f257d7e89f397ef7b6e
Naoki-Yokoyama-1991 <naoki.yokoyama.1991@gmail.com>
2021-01-26 17:35:13 +0200

    second commit

--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+
+import SongListItem from '../components/SongListItem';
+import albumDetails from '../data/albumDetails';
+import AlbumHeader from '../components/AlbumHeader';
 
 const AlbumScreen = () => {
   const route = useRoute();
@@ -11,7 +15,12 @@ const AlbumScreen = () => {
 
   return (
     <View>
-      <Text style={{ color: 'white' }}>Hello from Album Screen</Text>
+      <FlatList
+        data={albumDetails.songs}
+        renderItem={({ item }) => <SongListItem song={item} />}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={() => <AlbumHeader album={albumDetails} />}
+      />
     </View>
   );
 };
