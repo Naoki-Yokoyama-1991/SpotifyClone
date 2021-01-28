c151481ed4dbb1054e995ae81766e15c5da27ec2
Naoki-Yokoyama-1991 <naoki.yokoyama.1991@gmail.com>
2021-01-28 09:09:04 +0200

    compleate

--- components/SongListItem/index.tsx
+++ components/SongListItem/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Text, Image, View } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, Image, View, TouchableWithoutFeedback } from 'react-native';
 
 import styles from './styles';
 import { Song } from '../../types';
+import { AppContext } from '../../AppContext';
 
 export type SonListItemProps = {
   song: Song;
@@ -11,13 +12,21 @@ export type SonListItemProps = {
 const SongListItem = (props: SonListItemProps) => {
   const { song } = props;
 
+  const { setSongId } = useContext(AppContext);
+
+  const onPlay = () => {
+    setSongId(song.id);
+  };
+
   return (
     <View style={styles.container}>
-      <Image source={{ uri: song.imageUri }} style={styles.image} />
-      <View style={styles.rightContainer}>
-        <Text style={styles.title}>{song.title}</Text>
-        <Text style={styles.artist}>{song.artist}</Text>
-      </View>
+      <TouchableWithoutFeedback onPress={onPlay}>
+        <Image source={{ uri: song.imageUri }} style={styles.image} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{song.title}</Text>
+          <Text style={styles.artist}>{song.artist}</Text>
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 };
